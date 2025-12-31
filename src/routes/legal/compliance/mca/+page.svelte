73901b9704e2ab455adb384547100e3d16bcfcbe
Naman B Gor <devnamangor@gmail.com>
2025-12-31 17:10:52 +0530

    Add Legal/Compliance/MCA Page

---
+++ src/routes/legal/compliance/mca/+page.svelte
@@ -0,0 +1,45 @@
+<svelte:head>
+  <title>Compliance - MCA</title>
+  <meta name="description" content="Compliance information for MCA" />
+  <meta property="og:image" content="https://nbrglm.com/images/logo.png" />
+  <meta property="og:url" content="https://nbrglm.com" />
+  <meta property="og:type" content="website" />
+  <meta property="og:site_name" content="NBRGLM Developers" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content="NBRGLM Developers" />
+  <meta
+    name="twitter:description"
+    content="NBRGLM Developers is a software development company that builds secure, scalable, and reliable software to power what's next."
+  />
+  <meta name="twitter:image" content="https://nbrglm.com/images/logo.png" />
+  <meta name="twitter:url" content="https://nbrglm.com" />
+  <meta name="twitter:site" content="@nbrglm" />
+  <meta name="twitter:creator" content="@nbrglm" />
+  <link rel="icon" href="/favicon.png" />
+  <link rel="apple-touch-icon" href="/favicon.png" />
+</svelte:head>
+
+<section
+  class="h-dvh flex flex-col justify-center items-center gap-2 p-4 md:p-8"
+>
+  <h1 class="text-4xl font-bold mb-4">MCA Compliance Information</h1>
+  <div class="max-w-3xl text-lg leading-relaxed">
+    <p class="mb-4">
+      NBRGLM Developers is committed to ensuring compliance with the regulations
+      set forth by the Ministry of Corporate Affairs (MCA). We adhere to all
+      applicable laws and guidelines to maintain transparency and accountability
+      in our operations.
+    </p>
+    <p class="mb-4">The annual returns can be found as per below:</p>
+    <ul class="list-disc list-inside mb-4">
+      <li class="mb-2">
+        <a
+          href="https://www.nbrglm.com/legal/compliance/mca/annual-return/fy2024-25.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="text-blue-600 hover:underline">Annual Return FY2024-25</a
+        >
+      </li>
+    </ul>
+  </div>
+</section>
